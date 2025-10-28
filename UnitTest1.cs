afe84da05800308d241ba5422290afef61c849c4
Alejandro <Alejandro@DESKTOP-J00B70I>
2025-10-27 20:13:26 -0600

    Agregar archivos de proyecto.

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace Proyecto_Auto
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
