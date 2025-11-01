a133b6e4d87256884de016834580f240c1198910
Alejandro <esojandro@gmail.com>
2025-10-31 19:19:52 -0600

    Mi primer request

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,17 +1,32 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+
 namespace Proyecto_Auto
 {
     public class Tests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
-
+            // Llamado al driver que a a utilizar
+            var options = new EdgeOptions();
+            driver = new EdgeDriver(options);
+            driver.Manage().Window.Maximize();
         }
 
         [Test]
-        public void Test1()
+        public void TestNavegar()
         {
-            Assert.Pass();
+            driver.Navigate().GoToUrl("https://www.bing.com");
+        }
+
+        [TearDown]
+
+        public void TearDown() 
+        { 
+            driver.Quit();
         }
     }
 }
