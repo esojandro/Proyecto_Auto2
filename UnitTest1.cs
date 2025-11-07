de29a5adf3b4126efc2ec2e1a3b3d8698dfbbda5
Alejandro <esojandro@gmail.com>
2025-11-06 20:22:09 -0600

    Se estuvieron agregamdo más test cases

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Edge;
+using NUnit.Framework;
 
 namespace Proyecto_Auto
 {
@@ -20,6 +21,36 @@ namespace Proyecto_Auto
         public void TestNavegar()
         {
             driver.Navigate().GoToUrl("https://www.bing.com");
+        }
+
+        [Test]
+        public void loginExitoso()
+        {
+            driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login");
+            driver.FindElement(By.Id("username")).SendKeys("student");
+            driver.FindElement(By.Id("password")).SendKeys("Password123");
+            driver.FindElement(By.Id("submit")).Click();
+            Assert.That(driver.FindElement(By.CssSelector("p[class='has-text-align-center'] strong")).Displayed, Is.True);
+        }
+
+        [Test]
+        public void loginErroneoPass()
+        {
+            driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login");
+            driver.FindElement(By.Id("username")).SendKeys("student");
+            driver.FindElement(By.Id("password")).SendKeys("123");
+            driver.FindElement(By.Id("submit")).Click();
+            Assert.That(driver.FindElement(By.CssSelector("#error")).Displayed, Is.True);
+        }
+
+        [Test]
+        public void loginErroneoUsr()
+        {
+            driver.Navigate().GoToUrl("https://practicetestautomation.com/practice-test-login");
+            driver.FindElement(By.Id("username")).SendKeys("studt");
+            driver.FindElement(By.Id("password")).SendKeys("Password123");
+            driver.FindElement(By.Id("submit")).Click();
+            Assert.That(driver.FindElement(By.CssSelector("#error")).Displayed, Is.True);
         }
 
         [TearDown]
